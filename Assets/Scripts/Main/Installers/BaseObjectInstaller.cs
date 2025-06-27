9537aa7ede2473871e5b5aee93cce6ab4740b712
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-06-27 11:41:52 +0500

    add base installers

---
+++ Assets/Scripts/Main/Installers/BaseObjectInstaller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace CastleFight.Main.Installers
+{
+    public abstract class BaseObjectInstaller : MonoInstaller
+    {
+        [SerializeField] private bool _asSingle = true;
+
+        public bool AsSingle => _asSingle;
+    }
+}
