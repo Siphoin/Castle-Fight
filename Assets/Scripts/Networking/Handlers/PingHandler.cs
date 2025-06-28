89e22ab82683fb48e83fcc4122f8b7463d456c18
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-06-28 19:20:57 +0500

    improve ping handler

--- Assets/Scripts/Networking/Handlers/PingHandler.cs
+++ Assets/Scripts/Networking/Handlers/PingHandler.cs
@@ -4,6 +4,8 @@ using Unity.Netcode.Transports.UTP;
 using UniRx;
 using Cysharp.Threading.Tasks;
 using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 
 namespace CastleFight.Networking.Handlers
 {
@@ -16,9 +18,9 @@ namespace CastleFight.Networking.Handlers
         public IObservable<int> OnPingChanged => _onPingChanged;
 
         [SerializeField] private float _pingCheckInterval = 1.0f;
-
+        private Stopwatch _pingStopwatch = new Stopwatch();
+        private bool _rpcReceived;
         private int _lastPing = -1;
-        private bool _running;
 
         private void Awake()
         {
@@ -33,13 +35,48 @@ namespace CastleFight.Networking.Handlers
             {
                 if (IsClient)
                 {
-                    var pingMs = NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetCurrentRtt(NetworkManager.Singleton.NetworkConfig.NetworkTransport.ServerClientId);
-
-                     Debug.Log("Measured Ping: " + pingMs);
+                    await MeasurePingAsync();
                 }
 
                 await UniTask.Delay(TimeSpan.FromSeconds(_pingCheckInterval), ignoreTimeScale: true);
             }
+        }
+
+        private async UniTask MeasurePingAsync()
+        {
+            _pingStopwatch.Restart();
+            _rpcReceived = false;
+            PingServerRpc();
+
+            try
+            {
+                // Ожидаем получения Pong с таймаутом 5 секунд
+                await UniTask.WaitUntil(() => _rpcReceived)
+                    .Timeout(TimeSpan.FromSeconds(5));
+
+                _pingStopwatch.Stop();
+                int pingMs = (int)_pingStopwatch.ElapsedMilliseconds / 2;
+                _onPingChanged.OnNext(pingMs);
+                Debug.Log($"Ping: {pingMs}ms");
+            }
+            catch (TimeoutException)
+            {
+                Debug.LogWarning("Ping measurement timed out");
+                _onPingChanged.OnNext(-1);
+            }
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void PingServerRpc(ServerRpcParams rpcParams = default)
+        {
+            var clientId = rpcParams.Receive.SenderClientId;
+            PongClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new[] { clientId } } });
+        }
+
+        [ClientRpc]
+        private void PongClientRpc(ClientRpcParams rpcParams = default)
+        {
+            _rpcReceived = true;
         }
     }
 }
