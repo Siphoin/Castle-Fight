3970b84c5b27dce10d870138d2eeaf01f127ce1f
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-06-29 08:49:51 +0500

    fix unit animator sync

---
+++ Assets/Scripts/Networking/Components/OwnerNetworkAnimator.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Netcode.Components;
+
+namespace CastleFight.Networking.Components
+{
+    public class OwnerNetworkAnimator : NetworkAnimator
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
